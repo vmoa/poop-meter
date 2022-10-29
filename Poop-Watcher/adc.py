da49fa92479704033b137504c99ac6022a822c12
David Kensiski <david@kensiski.org>
2022-10-29 18:38:38 +0100

    use actual cs pin we wired; add differentailChannel() method

--- Poop-Watcher/adc.py
+++ Poop-Watcher/adc.py
@@ -1,6 +1,7 @@
 #
 # adc.py -- Class to read the poop meter via SPI from the ADC (MCP3008)
 #
+# Uses CircuitPython mcp3xxx library (which includes Blinka):
 # https://learn.adafruit.com/mcp3008-spi-adc/python-circuitpython
 #
 
@@ -18,16 +19,20 @@ class Adc:
     def __init__(self):
         """Initialize the spi bus and chip select and create the mcp object."""
         self.spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-        self.cs = digitalio.DigitalInOut(board.D5)
+        self.cs = digitalio.DigitalInOut(board.D8)  # GPIO8 == CE0
         self.mcp = MCP.MCP3008(self.spi, self.cs)
 
     def channel(pin=MCP.P0):
-        # create an analog input channel
+        """Create a single-ended analog input channel on pin."""
         self.chan = AnalogIn(self.mcp, pin)
+
+    def differentailChannel(pin1=MCP.P0, pin2=MCP.P1):
+        """Create a differential analgon input channel between pins1 and pins2.
+           Note that not all combinations are possible; see MCP3008 documentation."""
+        self.chan = AnalogIn(mcp, pin1, pin2)
 
     def value(self):
         return(self.chan.value)
 
     def voltage(self):
         return(self.chan.voltage)
-
