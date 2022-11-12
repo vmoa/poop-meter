b75a78bf0f353dea34888f03f5b44b8f4924e0ef
David Kensiski (macbookpro) <dkensiski@dkensiski-macbookpro.roam.corp.google.com>
2022-11-12 06:59:38 -0800

    checkpoint: add adc averaging

--- Poop-Watcher/adc.py
+++ Poop-Watcher/adc.py
@@ -15,6 +15,10 @@ import pager
 
 class Adc:
 
+    val_array = []
+    voltage_array = []
+    num_samples = 10
+
     def __init__(self):
         """Initialize the spi bus and chip select and create the mcp object."""
         self.spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
@@ -25,9 +29,23 @@ class Adc:
         # create an analog input channel
         self.chan = AnalogIn(self.mcp, pin)
 
-    def value(self):
+    def abs_value(self):
         return(self.chan.value)
 
-    def voltage(self):
+    def abs_voltage(self):
         return(self.chan.voltage)
+
+    def average(self, array, sample):
+        """Prepend new sample to the array, turncate at num_samples, and return the average."""
+        array.insert(0, sample)     # TODO: Should probably do something to ignore crazy outliers
+        del(array[num_samples:])
+        return(round(mean(array)))
+
+    def value(self):
+        """Return the average value over the last <num_samples>."""
+        return(self.average(val_array, self.abs_value()))
+
+    def voltage(self):
+        """Return the average voltage over the last <num_samples>."""
+        return(self.average(voltage_array, self.abs_voltage()))
 
