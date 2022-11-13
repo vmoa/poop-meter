e3c36600f0b095a6e3d203a916f94dca156bc005
David Kensiski <david@kensiski.org>
2022-11-13 00:05:07 +0000

    get_{value,voltage}() return average over <num_samples>

--- Poop-Watcher/adc.py
+++ Poop-Watcher/adc.py
@@ -10,6 +10,7 @@ import digitalio
 import board
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
+from statistics import mean
 
 import pager
 
@@ -19,14 +20,18 @@ class Adc:
     voltage_array = []
     num_samples = 10
 
+    full_value = 65535   # Value returned when the tank is ready to burst
+
     def __init__(self):
         """Initialize the spi bus and chip select and create the mcp object."""
         self.spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
         self.cs = digitalio.DigitalInOut(board.D5)
         self.mcp = MCP.MCP3008(self.spi, self.cs)
+        self.channel()
 
-    def channel(pin=MCP.P0):
+    def channel(self, pin=MCP.P0):
         # create an analog input channel
+        print("Adc.channel: setting to {}".format(pin))
         self.chan = AnalogIn(self.mcp, pin)
 
     def abs_value(self):
@@ -38,14 +43,18 @@ class Adc:
     def average(self, array, sample):
         """Prepend new sample to the array, turncate at num_samples, and return the average."""
         array.insert(0, sample)     # TODO: Should probably do something to ignore crazy outliers
-        del(array[num_samples:])
+        del(array[self.num_samples:])
         return(round(mean(array)))
 
-    def value(self):
+    def get_value(self):
         """Return the average value over the last <num_samples>."""
-        return(self.average(val_array, self.abs_value()))
+        return(self.average(self.val_array, self.abs_value()))
 
-    def voltage(self):
+    def get_voltage(self):
         """Return the average voltage over the last <num_samples>."""
-        return(self.average(voltage_array, self.abs_voltage()))
+        return(self.average(self.voltage_array, self.abs_voltage()))
+
+    def get_percent(self):
+        """Return the percentage full using averaged <value> as index."""
+        return(self.get_value() / self.full_value * 100)
 
