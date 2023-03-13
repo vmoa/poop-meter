d64c6718545d1e80ae46de133b3bb244992e8fd2
David Kensiski <david@kensiski.org>
2023-03-13 00:42:11 +0000

    first stab at business logic for incremental paging

--- Poop-Watcher/pager.py
+++ Poop-Watcher/pager.py
@@ -4,7 +4,10 @@
 
 import configparser
 import logging
+import time
 import twilio.rest
+
+import device
 
 class Pager:
 
@@ -62,7 +65,92 @@ class Pager:
         else:
             logging.error('Pager.send(): cannot send to {}: no Twilio config found'.format(recip))
 
+    #
+    # Generic-ish Twilio stuff above; poopy stuff below
+    #
+
+    lastSend = {}
+
     @classmethod
-    def notify(cls, msg, varags, recip=None):
-        """Format and send standard message according to defined schedule."""
-        pass
+    def notify(cls, cat, msg, recip=None):
+        """Send a categorical message <cat> with protection to prevent rapid duplication."""
+        now = int(time.time())
+        if (cat in lastSend):
+            elapsed = now - lastSend[cat]
+            if (elapsed > 10 * min):
+                cls.send(msg, recip)
+                lastSend[cat] = now
+        else:
+            cls.send(msg, recip)
+            lastSend[cat] = now
+
+    # Based on bench testing an inch equates to about 8 ticks on the ADC
+    # https://docs.google.com/document/d/1y9-7Vs1QebsVzuF8hXrv6W9YnSJAdl80ODEjWZSmrM8/
+    # This should be tuned after some real-world testing
+    hour = 3600
+    poopmap = [
+        {
+            'severity': 'empty',
+            'threshold': -1,            # Tune `nominal` so this only happens after a pump-out
+            'frequency': 7 * 24 * hour,
+        }, {
+            'severity': 'nominal',
+            'threshold': 320,           # 60 inches below 'PANIC' (5 feet) -- to be tuned
+            'frequency': 7 * 24 * hour,
+        }, {
+            'severity': 'High',
+            'threshold': 700,           # Should match the Classroom Red Light turning on
+            'frequency': 24 * hour,     # Wholly arbitrary before real-world tuning
+        }, {
+            'severity': 'URGENT',
+            'threshold': 800,           # 20 inches below sensor
+            'frequency': 4 * hour,      # Or maybe this should be the Red Light?
+        }, {
+            'severity': 'PANIC',        # This will trigger shutting off the water main valve
+            'threshold': 880,           # 10 inches below sensor
+            'frequency': 1 * hour,
+        }, {
+            'severity': 'placeholder9', # place holder to make the math easier
+            'threshold': 99999,         # should never be returned
+            'frequency': 1 * hour,      # but just in case...
+        }
+    ]
+
+    @classmethod
+    def get_panic(cls):
+        for a in cls.poopmap:
+            if (a["severity"] == 'PANIC'):
+                return(a["threshold"])
+        logging.error("pager.get_panic(): cannot find panic!")
+
+    @classmethod
+    def get_nominal(cls):
+        for a in cls.poopmap:
+            if (a["severity"] == 'nominal'):
+                return(a["threshold"])
+        logging.error("pager.get_nominal(): cannot find nominal!")
+
+    poopmessage = "Poop level {}: {}% ({}, {}v)"  # Poop level PANIC|URGENT|High|nominal: 93% (128, 4.12v)
+    last_poopalert = poopmap[0]
+    last_pooptime = 0
+
+    @classmethod
+    def poop_notify(cls, value, voltage, percent, recip=None):
+        """Format and send poop message, throttled according to `frequency` defined in poopmap."""
+        now = int(time.time())
+
+        # Find approprate stanza
+        for p in range(len(cls.poopmap)):
+            if (value < cls.poopmap[p]["threshold"]):
+                alert = cls.poopmap[p-1]
+                break
+
+        elapsed = now - cls.last_pooptime
+
+        if ((p != cls.last_poopalert) or (elapsed > alert["frequency"])):
+            cls.send(cls.poopmessage.format(alert["severity"], percent, value, voltage))
+            cls.last_poopalert = p
+            cls.last_pooptime = now
+
+
+
