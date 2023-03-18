d7748ec57791a1b76fe2a68efb4785e4f521ba6b
David Kensiski <david@kensiski.org>
2023-03-18 17:18:13 +0000

    fixed message format; moved poopmap

--- Poop-Watcher/pager.py
+++ Poop-Watcher/pager.py
@@ -17,6 +17,38 @@ class Pager:
     #client                                 # Twilio client; created in __init__()
     simulate = False                        # If set, simulate sending pages but don't actually send
 
+    # Based on bench testing an inch equates to about 8 ticks on the ADC
+    # https://docs.google.com/document/d/1y9-7Vs1QebsVzuF8hXrv6W9YnSJAdl80ODEjWZSmrM8/
+    # This should be tuned after some real-world testing
+    hour = 3600
+    poopmap = [
+        {
+            'severity': 'empty',
+            'threshold': -1,            # Tune `nominal` so this only happens after a pump-out
+            'frequency': 7 * 24 * hour,
+        }, {
+            'severity': 'nominal',
+            'threshold': 320,           # 60 inches below 'PANIC' (5 feet) -- to be tuned
+            'frequency': 7 * 24 * hour,
+        }, {
+            'severity': 'High',
+            'threshold': 700,           # Should match the Classroom Red Light turning on
+            'frequency': 24 * hour,     # Wholly arbitrary before real-world tuning
+        }, {
+            'severity': 'URGENT',
+            'threshold': 800,           # 20 inches below sensor
+            'frequency': 4 * hour,      # Or maybe this should be the Red Light?
+        }, {
+            'severity': 'PANIC',        # This will trigger shutting off the water main valve
+            'threshold': 880,           # 10 inches below sensor
+            'frequency': 1 * hour,
+        }, {
+            'severity': 'placeholder9', # place holder to make the math easier
+            'threshold': 99999,         # should never be returned
+            'frequency': 1 * hour,      # but just in case...
+        }
+    ]
+
     def __init__(self, simulate=False):
         self.simulate_mode(simulate)
         self.config.read('twilio.conf')
@@ -33,6 +65,8 @@ class Pager:
     def simulate_mode(cls, simulate):
         """Enable or disable simulate mode and sending of actual pages."""
         cls.simulate = simulate
+        for entry in cls.poopmap:
+            entry["frequency"] = 10  # Override all alerts to 10 seconds
         if (simulate):
             logging.info("Pager in simulate mode -- pages will NOT be sent")
 
@@ -69,10 +103,11 @@ class Pager:
     # Generic-ish Twilio stuff above; poopy stuff below
     #
 
+    # Message categories
     lastSend = {}
 
     @classmethod
-    def notify(cls, cat, msg, recip=None):
+    def notify_maybe(cls, cat, msg, recip=None):
         """Send a categorical message <cat> with protection to prevent rapid duplication."""
         now = int(time.time())
         if (cat in lastSend):
@@ -83,38 +118,6 @@ class Pager:
         else:
             cls.send(msg, recip)
             lastSend[cat] = now
-
-    # Based on bench testing an inch equates to about 8 ticks on the ADC
-    # https://docs.google.com/document/d/1y9-7Vs1QebsVzuF8hXrv6W9YnSJAdl80ODEjWZSmrM8/
-    # This should be tuned after some real-world testing
-    hour = 3600
-    poopmap = [
-        {
-            'severity': 'empty',
-            'threshold': -1,            # Tune `nominal` so this only happens after a pump-out
-            'frequency': 7 * 24 * hour,
-        }, {
-            'severity': 'nominal',
-            'threshold': 320,           # 60 inches below 'PANIC' (5 feet) -- to be tuned
-            'frequency': 7 * 24 * hour,
-        }, {
-            'severity': 'High',
-            'threshold': 700,           # Should match the Classroom Red Light turning on
-            'frequency': 24 * hour,     # Wholly arbitrary before real-world tuning
-        }, {
-            'severity': 'URGENT',
-            'threshold': 800,           # 20 inches below sensor
-            'frequency': 4 * hour,      # Or maybe this should be the Red Light?
-        }, {
-            'severity': 'PANIC',        # This will trigger shutting off the water main valve
-            'threshold': 880,           # 10 inches below sensor
-            'frequency': 1 * hour,
-        }, {
-            'severity': 'placeholder9', # place holder to make the math easier
-            'threshold': 99999,         # should never be returned
-            'frequency': 1 * hour,      # but just in case...
-        }
-    ]
 
     @classmethod
     def get_panic(cls):
@@ -130,7 +133,7 @@ class Pager:
                 return(a["threshold"])
         logging.error("pager.get_nominal(): cannot find nominal!")
 
-    poopmessage = "Poop level {}: {}% ({}, {}v)"  # Poop level PANIC|URGENT|High|nominal: 93% (128, 4.12v)
+    poopmessage = "Poop level {}: {:1.0f}% ({:d}, {:3.2}v)"  # Poop level PANIC|URGENT|High|nominal: 93% (128, 4.12v)
     last_poopalert = poopmap[0]
     last_pooptime = 0
 
