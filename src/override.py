ebe1d87f44c7734fce7cdb4ef7a29bcb21b40987
David Kensiski (macbookpro) <dkensiski@dkensiski-macbookpro.roam.corp.google.com>
2023-04-28 06:58:34 -0700

    added first pass at override mode

--- src/override.py
+++ src/override.py
@@ -13,7 +13,42 @@ import grove
 import pager
 
 
+"""
+# Thoughts on Hard / Soft Override Mode
+
+## Soft Override Mode
+    Enter by holding momentary button for XX seconds
+    Exit by pressing momentary for XX seconds
+    Restart/reboot restores normal operation
+    PANIC level will restore normal operation
+## Hard Override Mode
+    Enter by toggling Emergency Override inside box; detect by Override low for > YY seconds
+    Can only be exitied by user restoring Emergency Override switch
+    Restart/reboot RETAINS Hard Override because it's a hardware switch
+
+Mode    Switch  Elapsed         Action
+none    no      d/c             -
+none    yes     <soft           -
+none    yes     <hard           => soft
+none    yes     >hard           => HARD
+soft    no      d/c             => SOFT
+soft    yes     <hard           -
+soft    yes     >hard           => HARD
+SOFT    no      -               -
+SOFT    yes     <soft           -
+SOFT    yes     >soft           => none
+HARD    no      d/c             => none
+HARD    yes     d/c             -
+
+"""
+
+
 class Override:
+
+    interval = {
+        'soft': 30,
+        'hard': 120,
+    }
 
     initialNotifySec = 300      # Seconds before sending first page about manual override (5 minutes grace)
     repeatNotifySec = 86400     # Seconds between subsequent pages about manual override (daily)
@@ -33,6 +68,66 @@ class Override:
             cls.repeatNotifySec = 10
 
     @classmethod
+    def detectMode(cls):
+        """Figure out which overrideMode we're in."""
+
+        # Mode    Switch  Elapsed         Action
+        # none    no      -               -
+        # none    yes     <soft           -
+        # none    yes     <hard           => soft
+        # none    yes     >hard           => HARD
+        # soft    no      -               => SOFT
+        # soft    yes     <hard           -
+        # soft    yes     >hard           => HARD
+        # SOFT    no      -               -
+        # SOFT    yes     <soft           -
+        # SOFT    yes     >soft           => none
+        # HARD    no      -               => none
+        # HARD    yes     -               -
+
+        if (device.Gpio.is_override.isOn()):
+            now = time.time()
+            if (!cls.overrideStartTime):
+                cls.overrideStartTime = now
+            elapsed = cls.overrideStartTime - now
+
+            match cls.mode:
+                case "none":
+                    if (elapsed < cls.modeTime["soft"]):
+                        pass
+                    elif (elapsed < cls.modeTime["hard"]):
+                        cls.mode = 'soft'
+                        logging.debug("override.detectMode() ==> soft after {} seconds".format(elapsed))
+                    else:
+                        cls.mode = 'HARD'
+                        logging.debug("override.detectMode() ==> HARD* after {} seconds".format(elapsed))
+
+                case "soft":
+                    if (elapsed < cls.modeTime["hard"]):
+                        pass
+                    else:
+                        cls.mode = 'HARD'
+                        logging.debug("override.detectMode() ==> HARD after {} seconds".format(elapsed))
+
+                case "SOFT":
+                    pass
+
+                case "HARD":
+                    pass
+
+        else  # device.Gpio.is_override.isOff()
+            if (cls.overrideStartTime):
+                elapsed = cls.overrideStartTime - time.time()
+                if (cls.mode = 'soft'):
+                    cls.mode = 'SOFT'
+                    logging.debug("override.detectMode() ==> SOFT after {} seconds".format(elapsed))
+                elif (cls.mode = 'HARD' or cls.mode = 'SOFT'):
+                    cls.mode = 'none'
+                    logging.debug("override.detectMode() ==> none after {} seconds".format(elapsed))
+                cls.overrideStartTime = None
+
+
+    @classmethod
     def notifyMaybe(cls):
         """Send override notification if it's time."""
         now = time.time()
@@ -48,6 +143,7 @@ class Override:
         now = time.time()
         if (device.Gpio.is_override.isOn()):
             if (cls.startTime > 0):
+                cls.detectMode()
                 cls.notifyMaybe()
             else:
                 logging.warning("Manual override is enabled; will notify in {} seconds if still set".format(cls.initialNotifySec))
